c9b729d4af7a6be17326d509975d5d71ca36326e
Nsubuga Nasif <nasifun@gmail.com>
2025-06-10 06:27:30 +0200

    my python beginner 2025
    Python school 6 hours a day 6 day

---
+++ type_casting.py
@@ -0,0 +1,16 @@
+# Typecasting is the process of converting a variable from one type to another.
+#        str(), int(), float(), bool()
+
+#                    string to text
+number = 7
+text = str(number)
+print('lucky number: ' + text)
+
+#                    integer to string
+s = '10'
+n = int(s)
+print(n * 3)
+
+#                    text to float
+
+
---
+++ var_str_int_f_boolean.py
@@ -0,0 +1,96 @@
+
+# strings with variable 
+
+first_name = 'Nasif'
+food = 'pizza'
+email = 'nasifun@gmail.com'
+car = 'naz'
+terea = 'smoke'
+
+
+print(first_name)
+print(f'hello {first_name}')
+print(f'send me the email {email}')
+print(f'you like {food}')
+print(f'start when 01 {car}')
+print(f'stop smoking its to extensive {terea}')
+
+# integers
+age = 36
+quantity = 5 
+num_of_students = 20
+
+print(f'you are {age} years old')
+print(f'you are buying {quantity} items')
+print(f'they are {num_of_students} kids in your class')
+
+
+#float 
+
+price = 10.99
+gpa = 3.2 
+distance = 5.5 
+
+print(f'your price is ${price}')
+print(f'your gpa is {gpa}')
+print(f'your distance is {distance}km')
+
+#boolean 
+
+for_sale = True
+is_student = False
+
+if is_student:
+    print('your are a student')
+else:
+    print('your NOT a student')
+
+if for_sale:
+    print('the item is available')
+else:
+    print('that item is NOT available')
+
+# Assignment 
+# of variables, strings, integers, float, boolean 
+# 1 variable
+
+name = 'Nasif'
+colour = 'blue'
+country = 'kenya'
+
+message = f""" 
+Hi my name is {name}!
+my favorite colour is {colour}.
+and i was born in {country}.
+"""
+print(message)
+
+#strings
+
+name = 'Nasif'
+colour = 'blue'
+
+print(f'Hi am {name} and my favorite colour is {colour}')
+
+# integer 
+
+age = 36 
+
+print('in 5 years, I will be', age + 5)
+
+#float 
+
+height = 1.75 
+
+print('My height is', height,'meters')
+
+# Boolean 
+
+is_student = False
+
+if is_student:
+    print('Your are a student')
+else:
+    print('Your are not a student')
+    
+
